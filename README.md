fde6726dc1e1b6276d99dac067639996dc807381
shmador <dorattar4@gmail.com>
2025-05-10 16:50:40 +0300

    Added files

---
+++ README.md
@@ -0,0 +1,1 @@
+Gathering network data and putting on a DynamoDB table. Using AWS Lambda with an API GW to trigger the data update.
---
+++ script.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+source .env
+
+# detect interface & measure bytes/sec
+IF="${1:-$(ip route get 8.8.8.8 | awk '/dev/ {print $5; exit}')}"
+INTERVAL="${2:-1}"
+rx1=$(< /sys/class/net/"$IF"/statistics/rx_bytes)
+tx1=$(< /sys/class/net/"$IF"/statistics/tx_bytes)
+sleep "$INTERVAL"
+rx2=$(< /sys/class/net/"$IF"/statistics/rx_bytes)
+tx2=$(< /sys/class/net/"$IF"/statistics/tx_bytes)
+rx_bps=$(( (rx2 - rx1) / INTERVAL ))
+tx_bps=$(( (tx2 - tx1) / INTERVAL ))
+id_name="dor-auto${VERSION}"
+((VERSION++))
+sed -i "s/^VERSION=.*/VERSION=$VERSION/" .env
+# Build JSON with printf
+JSON=$(printf \
+  '{"TableName":"imtech","Item":{"id":{"S":"%s"},"interface":{"S":"%s"},"rx_bps":{"N":"%d"},"tx_bps":{"N":"%d"}}}' \
+  "$id_name" "$IF" "$rx_bps" "$tx_bps"
+)
+
+# Send it
+URL="https://mj92zct6nc.execute-api.il-central-1.amazonaws.com/default/imtech-dor-py" 
+curl -X POST $URL -H "Content-Type: application/json" -d "$JSON"
+
+# Debug print
+echo "Sent JSON: $JSON"
+
